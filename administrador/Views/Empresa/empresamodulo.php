bfc73401376bf085719298c0c3cf24c256b78c1b
root <root@centos9.localhost>
2024-04-24 12:28:02 -0500

    FORMATO EMPRESSA

--- administrador/Views/Empresa/empresamodulo.php
+++ administrador/Views/Empresa/empresamodulo.php
@@ -1,80 +1,103 @@
-<?php 
-    adminHeader($data);
-    adminMenu($data); 
-    //filelang(Setlanguage,"general") 
-    //require_once "Views/Salon/Modals/modalSalon.php";
+<?php
+adminHeader($data);
+adminMenu($data);
+//filelang(Setlanguage,"general") 
+//require_once "Views/Salon/Modals/modalSalon.php";
 ?>
-    <div id="contentAjax"></div> 
-    <main class="app-content">
-      <div class="app-title">
-        <div>
-            <h1><i class="fas fa-user-tag"></i> <?= $data['page_title'] ?></h1>
-        </div>
-        <ul class="app-breadcrumb breadcrumb">
-          <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-          <li class="breadcrumb-item"><a href="<?= base_url(); ?>/<?= $data['page_back'] ?>"><?= $data['page_title'] ?></a></li>
-        </ul>
-      </div>
+<div id="contentAjax"></div>
+<main class="app-content">
+  <div class="app-title">
+    <div>
+      <h1><i class="fas fa-user-tag"></i> <?= $data['page_title'] ?></h1>
+    </div>
+    <ul class="app-breadcrumb breadcrumb">
+      <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
+      <li class="breadcrumb-item"><a href="<?= base_url(); ?>/<?= $data['page_back'] ?>"><?= $data['page_title'] ?></a>
+      </li>
+    </ul>
+  </div>
+  <input type="hidden" id="txth_ids" name="txth_ids" value="">
 
-
-      <div class="row">
-        <div class="col-md-3">
-          <div class="tile row">
-              <div class="form-group col-md-12">
-                <label for="cmb_empresa">Empresa</label>
-                <select class="form-control" id="cmb_empresa" name="cmb_empresa" onchange="fntInstructor(this.value)">
-                  <?php
-                  echo '<option value="0">SELECCIONAR</option>';
-                  foreach ($data['Empresas'] as $opcion) {
-                    echo '<option value="' . $opcion['Ids'] . '" >' . $opcion['NombreComercial'] . '</option>';
-                  }
-                  ?>
-                </select>
-              </div>
-              <div class="form-group col-md-12">
-                <label for="cmb_Emp_modulos">Presione CTRL y haga clic para seleccionar varias opciones a la vez.</label>
-                <select class="form-control" multiple id="cmb_Emp_modulos" name="cmb_Emp_modulos" >
-                <?php
-                  foreach ($data['EmpModulo'] as $opcion) {
-                    echo '<option value="' . $opcion['Ids'] . '" >' . $opcion['Nombre'] . '</option>';
-                  }
-                  ?>
-                </select>
-              </div>
-              <div class="form-group col-md-12">
-                <label for="cmb_modulos">Presione CTRL y haga clic para seleccionar varias opciones a la vez.</label>
-                <select class="form-control" multiple id="cmb_modulos" name="cmb_modulos" >
-                <?php
-                  foreach ($data['Modulos'] as $opcion) {
-                    echo '<option value="' . $opcion['Ids'] . '" >' . $opcion['Nombre'] . '</option>';
-                  }
-                  ?>
-                </select>
-              </div>
-              
-              <div class="form-group col-md-12">
-                <button type="button" class="btn btn-dark" id="btn_buscar"><i class="fa fa-magnifying-glass"></i>Buscar</button>
-                <button type="button" class="btn btn-success" id="btn_imprimir"><i class="fa fa-print"></i>Imprimir</button>
-              </div>
-              
+  <div class="row">
+    <div class="col-md-6">
+      <div class="tile">
+        <div class="tile-title-w-btn">
+          <div class="form-group">
+            <label for="cmb_empresa">
+              <h3 class="title">Empresa Módulo</h3>
+            </label>
+            <select class="form-control" id="cmb_empresa" name="cmb_empresa" onchange="fntInstructor(this.value)">
+              <?php
+              echo '<option value="0">SELECCIONAR</option>';
+              foreach ($data['Empresas'] as $opcion) {
+                echo '<option value="' . $opcion['Ids'] . '" >' . $opcion['NombreComercial'] . '</option>';
+              }
+              ?>
+            </select>
           </div>
         </div>
-
-
-        <div class="col-md-9">
-          <div class="tile">
-            <div id="list_tables">
-              <h3 class="tile-title">Horarios</h3>
+        <div class="tile-body">
+          <div class="row">
+            <div class="form-group col-md-12">
+              <h5>Presione CTRL y haga clic para seleccionar varias opciones a la vez.</h5>
             </div>
-            
-            
+          </div>
+          <div class="row">
+            <div class="form-group col-md-5">
+              <select class="form-control" multiple id="cmb_modulos" name="cmb_modulos" style="height: 300px;">
+                <?php
+                foreach ($data['Modulos'] as $opcion) {
+                  echo '<option value="' . $opcion['Ids'] . '" >' . $opcion['Nombre'] . '</option>';
+                }
+                ?>
+              </select>
+            </div>
+            <div class="form-group col-md-2 align-items-center" >
+              <button type="button" class="btn btn-dark" id="btn_generar"><i class="fa fa-fast-forward"
+                  aria-hidden="true">&nbsp;&nbsp;TODO</i>
+              </button>
+              <button type="button" class="btn btn-dark" id="btn_generar"><i class="fa fa-step-forward"
+                  aria-hidden="true">&nbsp;&nbsp;UNO</i>
+              </button>
+              <button type="button" class="btn btn-dark" id="btn_generar"><i class="fa fa-fast-backward"
+                  aria-hidden="true">&nbsp;&nbsp;TODO</i>
+              </button>
+              <button type="button" class="btn btn-dark" id="btn_generar"><i class="fa fa-step-backward"
+                  aria-hidden="true">&nbsp;&nbsp;UNO</i>
+              </button>
+            </div>
+            <div class="form-group col-md-5">
+
+              <select class="form-control" multiple id="cmb_Emp_modulos" name="cmb_Emp_modulos" style="height: 300px;">
+                <?php
+                foreach ($data['EmpModulo'] as $opcion) {
+                  echo '<option value="' . $opcion['Ids'] . '" >' . $opcion['Nombre'] . '</option>';
+                }
+                ?>
+              </select>
+            </div>
           </div>
         </div>
       </div>
+    </div>
+    <div class="col-md-6">
+      <div class="tile">
+        <div class="tile-title-w-btn">
+          <h3 class="title">SweetAlert</h3>
+          <p><a class="btn btn-primary icon-btn" href="http://t4t5.github.io/sweetalert/" target="_blank"><i
+                class="fa fa-file"></i>Docs</a></p>
+        </div>
+        <div class="tile-body">
+          <p>This plugin can be used as the replacement of native javascript alert, confirm and prompt functions.</p>
+          <h4>Demo</h4><a class="btn btn-info" id="demoSwal" href="#">Sample Alert</a>
+        </div>
+      </div>
+    </div>
+  </div>
 
-       
 
-         
-    </main>
+
+
+
+</main>
 <?php adminFooter($data); ?>
-    
